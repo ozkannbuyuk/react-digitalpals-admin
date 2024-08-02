5d73902732a0a4aef77a94515b8f2372c2414344
ozkanbuyuk <ozkanbuyuk@icloud.com>
2024-08-02 14:57:34 +0300

    End

---
+++ src/constants/images.ts
@@ -0,0 +1,9 @@
+const images = {
+  lightLogo: require("../assets/images/digitalpals-logo.png"),
+  darkLogo: require("../assets/images/digitalpals-light-logo.png"),
+  pp: require("../assets/images/pp.jpeg"),
+  product: require("../assets/images/product.jpg"),
+  customer: require("../assets/images/customer.jpg"),
+};
+
+export default images;
